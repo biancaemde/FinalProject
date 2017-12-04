87e28cbf41a663006fe6813b8424d4b3ff69d84b
biancaemde <32073447+biancaemde@users.noreply.github.com>
2017-12-04 14:23:10 -0500

    cnn api

--- finalproject.py
+++ finalproject.py
@@ -1,2 +1,42 @@
-#Bianca Emde
-#SI 206 Final Project
+from pprint import pprint
+import json
+import requests
+
+#cnn
+# keyword = "trump"
+
+# params = {}
+# params['keyword'] = keyword
+# params['api_key'] = "" #put in api key
+
+# base_url = "https://services.cnn.com/newsgraph/search/"
+
+# r = requests.get(base_url, params)
+# responses = r.text
+# results = json.loads(responses)
+
+#pprint(results)
+
+
+#newsapi
+keyword = "trump"
+
+params = {}
+params['sources'] = "google-news"
+params['apiKey'] = "38a79e478d4b4e5886490773eb3454b5" 
+
+base_url = "https://newsapi.org/v2/top-headlines?"
+
+r = requests.get(base_url, params)
+responses = r.text
+results = json.loads(responses)
+
+
+description = results['articles'][0]['description']
+title = results['articles'][0]['title']
+
+#pprint(description)
+pprint(results)
+
+
+
